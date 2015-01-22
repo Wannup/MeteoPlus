7509c7690a4d08517106f15711fe71ab4e3829de
Erwan LE GUYADER <leguyader.erwan@gmail.com>
2015-01-22 23:20:11 +0100

    gestion de l'absence de connection et debut de la localisation sur la page d'accueil

--- app/src/main/java/com/example/erwan/meteoplus/MeteoActivity.java
+++ app/src/main/java/com/example/erwan/meteoplus/MeteoActivity.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -37,6 +43,9 @@ public class MeteoActivity extends ActionBarActivity  {
     private Button mCities;
     private ImageView weatherImg;
     private TextView temperature;
+
+    private double longitude;
+    private double latitude;
 
     private ArrayList<String> favorites;
     private SharedPreferences sharedPref;
@@ -98,10 +107,42 @@ public class MeteoActivity extends ActionBarActivity  {
         if(savedInstanceState != null){
             if(savedInstanceState.getString("actualCity") != null) {
                 this.cityDisplay = savedInstanceState.getString("actualCity");
-                displayMeteo(cityDisplay);
+                displayMeteo(cityDisplay, latitude, longitude);
             }
         }
+
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, mLocationListener);
+
+       /* if(cityDisplay==null){
+        //    displayMeteo("0", latitude, longitude);
+            displayMeteo("0", 2.3488, 48.853409);
+        }*/
+
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
@@ -116,108 +157,134 @@ public class MeteoActivity extends ActionBarActivity  {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            displayMeteo(data.getStringExtra("city_selected"));
+            displayMeteo(data.getStringExtra("city_selected"), latitude, longitude);
         }
     }
 
-    public void displayMeteo(String city) {
+    public void displayMeteo(String city, double lat, double lon) {
         // update the main content by replacing fragments
-        mFavButton.setVisibility(View.VISIBLE);
-        mFavButton.setChecked(false);
-        getXml(city);
 
-        switch(getWeather()){
-            case "01d":
-                weatherImg.setImageResource(R.drawable.sund);
-                break;
-
-            case "01n":
-                weatherImg.setImageResource(R.drawable.moonn);
-                break;
-
-            case "02d":
-                weatherImg.setImageResource(R.drawable.suncloud);
-                break;
-
-            case "02n":
-                weatherImg.setImageResource(R.drawable.mooncloud);
-                break;
-
-            case "03d":
-                weatherImg.setImageResource(R.drawable.cloud);
-                break;
-
-            case "03n":
-                weatherImg.setImageResource(R.drawable.cloud);
-                break;
-
-            case "04d":
-                weatherImg.setImageResource(R.drawable.darkcloud);
-                break;
-
-            case "04n":
-                weatherImg.setImageResource(R.drawable.darkcloud);
-                break;
-
-            case "09d":
-                weatherImg.setImageResource(R.drawable.rain);
-                break;
-
-            case "09n":
-                weatherImg.setImageResource(R.drawable.rain);
-                break;
-
-            case "10d":
-                weatherImg.setImageResource(R.drawable.suncloudrain);
-                break;
-
-            case "10n":
-                weatherImg.setImageResource(R.drawable.mooncloudrain);
-                break;
-
-            case "11d":
-                weatherImg.setImageResource(R.drawable.lightning);
-                break;
-
-            case "11n":
-                weatherImg.setImageResource(R.drawable.lightning);
-                break;
-
-            case "13d":
-                weatherImg.setImageResource(R.drawable.snow);
-                break;
-
-            case "13n":
-                weatherImg.setImageResource(R.drawable.snow);
-                break;
-
-            case "50d":
-                weatherImg.setImageResource(R.drawable.fog);
-                break;
-
-            case "50n":
-                weatherImg.setImageResource(R.drawable.fog);
-                break;
-            default:
-                weatherImg.setImageResource(0);
-                break;
-        }
-        if(getTemperature().charAt(0) == '-' && getTemperature().charAt(1) != '0') {
-            temperature.setText(getTemperature().charAt(0) + " " + getTemperature().charAt(1) + " °C");
-        } else {
-            if (getTemperature().charAt(1) == '0') {
-                temperature.setText(getTemperature().charAt(1) + " °C");
-            } else {
-                temperature.setText(getTemperature().charAt(0) + " °C");
-            }
-        }
-
-        meteoInfo.setText(Html.fromHtml("<b>" + getCityName() + "<br /><br /></br><small>Humidité: " + getHumidity() + " %<br /><br />Pression: " + getPressure() + " hPa<br /><br />Vitesse du vent: " + getWindSpeed() + " m/s</small>"));
-
-        if (checkItemInFavorites(city)){
-            mFavButton.setChecked(true);
-        } else {
+        if(isNetworkAvailable()) {
+            mFavButton.setVisibility(View.VISIBLE);
             mFavButton.setChecked(false);
+
+            Log.w("log", city);
+            Log.w("log", "" + lat);
+            Log.w("log", "" + lon);
+
+           if(!city.equals("0")) {
+                Log.w("log", "city");
+                getXmlWithCity(city);
+            } else {
+                Log.w("log", "loc");
+                getXmlWithLocation(lat, lon);
+            }
+
+            if(doc != null) {
+                switch (getWeather()) {
+                    case "01d":
+                        weatherImg.setImageResource(R.drawable.sund);
+                        break;
+
+                    case "01n":
+                        weatherImg.setImageResource(R.drawable.moonn);
+                        break;
+
+                    case "02d":
+                        weatherImg.setImageResource(R.drawable.suncloud);
+                        break;
+
+                    case "02n":
+                        weatherImg.setImageResource(R.drawable.mooncloud);
+                        break;
+
+                    case "03d":
+                        weatherImg.setImageResource(R.drawable.cloud);
+                        break;
+
+                    case "03n":
+                        weatherImg.setImageResource(R.drawable.cloud);
+                        break;
+
+                    case "04d":
+                        weatherImg.setImageResource(R.drawable.darkcloud);
+                        break;
+
+                    case "04n":
+                        weatherImg.setImageResource(R.drawable.darkcloud);
+                        break;
+
+                    case "09d":
+                        weatherImg.setImageResource(R.drawable.rain);
+                        break;
+
+                    case "09n":
+                        weatherImg.setImageResource(R.drawable.rain);
+                        break;
+
+                    case "10d":
+                        weatherImg.setImageResource(R.drawable.suncloudrain);
+                        break;
+
+                    case "10n":
+                        weatherImg.setImageResource(R.drawable.mooncloudrain);
+                        break;
+
+                    case "11d":
+                        weatherImg.setImageResource(R.drawable.lightning);
+                        break;
+
+                    case "11n":
+                        weatherImg.setImageResource(R.drawable.lightning);
+                        break;
+
+                    case "13d":
+                        weatherImg.setImageResource(R.drawable.snow);
+                        break;
+
+                    case "13n":
+                        weatherImg.setImageResource(R.drawable.snow);
+                        break;
+
+                    case "50d":
+                        weatherImg.setImageResource(R.drawable.fog);
+                        break;
+
+                    case "50n":
+                        weatherImg.setImageResource(R.drawable.fog);
+                        break;
+                    default:
+                        weatherImg.setImageResource(0);
+                        break;
+                }
+                if (getTemperature().charAt(0) == '-' && getTemperature().charAt(1) != '0') {
+                    temperature.setText(getTemperature().charAt(0) + " " + getTemperature().charAt(1) + " °C");
+                } else {
+                    if (getTemperature().charAt(1) == '0') {
+                        temperature.setText(getTemperature().charAt(1) + " °C");
+                    } else {
+                        temperature.setText(getTemperature().charAt(0) + " °C");
+                    }
+                }
+
+                meteoInfo.setText(Html.fromHtml("<b>" + getCityName() + "<br /><br /></br><small>Humidité: " + getHumidity() + " %<br /><br />Pression: " + getPressure() + " hPa<br /><br />Vitesse du vent: " + getWindSpeed() + " m/s</small>"));
+
+                if (checkItemInFavorites(city)) {
+                    mFavButton.setChecked(true);
+                } else {
+                    mFavButton.setChecked(false);
+                }
+            }else{
+                mFavButton.setVisibility(View.INVISIBLE);
+                weatherImg.setImageResource(0);
+                temperature.setText("");
+                meteoInfo.setText("Impossible de localiser.");
+            }
+        } else {
+            mFavButton.setVisibility(View.INVISIBLE);
+            weatherImg.setImageResource(0);
+            temperature.setText("");
+            meteoInfo.setText("Aucune connexion internet.");
         }
     }
 
@@ -271,10 +338,25 @@ public class MeteoActivity extends ActionBarActivity  {
     Get infos xml
      */
 
-    public void getXml(String city){
+    public void getXmlWithCity(String city){
 
-        // ajouter le réglage des unités de température et langue
-        String urlCityMeteo= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=xml&units=metric&lang=fr";
+        String urlCityMeteo= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=xml&units=metric&lang=fr&APPID=cd3f1c67011fafe48586264c65fb5d8f";
+
+        RetrieveWeatherXml xml = new RetrieveWeatherXml();
+        try {
+            this.doc = xml.execute(urlCityMeteo).get();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void getXmlWithLocation(double lat, double lon){
+
+        String urlCityMeteo= "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "mode=xml&units=metric&lang=fr&APPID=cd3f1c67011fafe48586264c65fb5d8f";
 
         RetrieveWeatherXml xml = new RetrieveWeatherXml();
         try {
@@ -370,5 +452,12 @@ public class MeteoActivity extends ActionBarActivity  {
         }
 
         return "Aucune infos";
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
